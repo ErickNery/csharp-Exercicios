94a9ae21e9ea977cdca0a191bc2e1e5e9a0d2ffc
Erick Nery <erick.a.a.nery@gmail.com>
2018-11-20 18:46:54 -0200

    adicionando exercicios iniciais

---
+++ Exercicios/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExerciciosIntroducaoPOO {
+    class Funcionario {
+
+        public string Nome { get; set; }
+        public double SalarioBruto { get; set; }
+        public double Desconto { get; set; }
+
+
+        public double SalarioLiquido() {
+            return this.SalarioBruto - this.Desconto;
+        }
+
+        public void AumentarSalario(double porcentagem) {
+            porcentagem = 1 + (porcentagem / 100);
+
+            SalarioBruto *= porcentagem;
+        }
+
+        public override string ToString() {
+            return string.Format("Dados do funcionario: {0}, R$ {1}", Nome, SalarioLiquido());
+        }
+    }
+}
---
+++ Exercicios/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+using System.Collections;
+
+
+namespace ExerciciosIntroducaoPOO {
+    class Main {
+
+        static void Main(string[] args) {
+
+            //Exercicio1();
+            //Exercicio2();
+            Exercicio3();
+
+            Console.ReadKey();
+        }
+
+        static void Exercicio1() {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Informe a altura: ");
+            retangulo.Altura = double.Parse(Console.ReadLine().Trim());
+            Console.WriteLine("Informe a largura: ");
+            retangulo.Largura = double.Parse(Console.ReadLine().Trim());
+            Console.WriteLine();
+            Console.WriteLine(retangulo);
+        }
+
+        static void Exercicio2() {
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine().Trim();
+            Console.Write("Salario Bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine().Trim());
+            Console.Write("Desconto: ");
+            funcionario.Desconto = double.Parse(Console.ReadLine().Trim());
+
+            Console.WriteLine();
+            Console.WriteLine(funcionario);
+            Console.WriteLine();
+            Console.Write("Deseja aumentar o salario em qual porcentagem: ");
+            funcionario.AumentarSalario(double.Parse(Console.ReadLine().Trim()));
+            Console.WriteLine();
+            Console.WriteLine(funcionario);
+        }
+
+        static void Exercicio3() {
+
+            string[] notas;
+            Aluno aluno = new Aluno();
+            aluno.Notas = new double[3];
+
+            Console.Write("Informe o nome do aluno: ");
+            aluno.Nome = Console.ReadLine().Trim();
+            Console.Write("Informe a 3 notas do aluno separadas por espaco: ");
+            notas = Console.ReadLine().Split(' ');
+            
+
+            for (int i = 0; i < notas.Length; i++) {
+                aluno.Notas[i] = double.Parse(notas[i]);
+            }
+
+            Console.WriteLine();
+            aluno.cauculaMedia();
+        }
+    }
+}
